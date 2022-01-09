562c6fa85f83e7d3793f3be0ed9e8471a381a219
djay-S <65866198+djay-S@users.noreply.github.com>
2022-01-09 13:29:38 +0530

    IMPLEMENT: click to scroll up

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import AboutMe from "./screens/aboutMe/aboutMe";
 import Projects from "./screens/projects/projects";
 import Sidenav from "./screens/sidenav/sidenav";
 import ContactMe from "./screens/contactMe/contactMe";
+import Scrolltop from "./screens/scrollTop/scrollTop";
 
 function App() {
   return (
     <div className="App">
       <Sidenav />
+      <Scrolltop />
       <Home />
       <AboutMe />
       <Projects />
