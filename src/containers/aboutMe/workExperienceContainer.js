9f14f04f65e82d9e81c9a576131b26cec622f7d6
djay-S <65866198+djay-S@users.noreply.github.com>
2022-03-06 17:28:54 +0530

    refactored experience sect

--- src/containers/aboutMe/workExperienceContainer.js
+++ src/containers/aboutMe/workExperienceContainer.js
@@ -14,10 +14,27 @@ const workExp = (props) => {
     return year;
   };
 
+  const getExperience = () => {
+    let dateDiffMilliSecs = props.toDate - props.fromDate;
+    const monthDiff = Math.round(dateDiffMilliSecs / (1000 * 3600 * 24 * 30));
+    let unit = "months";
+    let diff = monthDiff;
+    if (monthDiff >= 12) {
+      diff /= 12;
+      unit = "years";
+    }
+    if (diff === 1) {
+      unit = unit.slice(0, -1);
+    }
+    return diff + unit;
+  };
+
   return (
     <li className="work_exp" datatype={getDisplayYear()}>
       <h3 className="company">{props.company}</h3>
-      <h4 className="designation">{props.designation}</h4>
+      <h4 className="designation">
+        {props.designation}, {getExperience()}
+      </h4>
       <h5 className="location">{props.location}</h5>
       <span
         className="notes"
--- src/screens/aboutMe/workExperiences.js
+++ src/screens/aboutMe/workExperiences.js
@@ -5,7 +5,7 @@ import Education from "../../containers/aboutMe/educationContainer";
 const workExperience = () => {
   return (
     <div className="work_exps me_content">
-      <h2>Work Experience</h2>
+      <h2>My Background</h2>
       <ul className="contents">
         <WorkExperience
           designation={"Project Engineer"}
@@ -30,7 +30,24 @@ const workExperience = () => {
           location={"Chennai"}
           image={"wipro.png"}
           fromDate={new Date(2018, 8, 23)}
-          toDate={new Date(2019, 8, 1)}
+          toDate={new Date(2019, 8, 24)}
+          details={`
+          <ul>
+          <li>Worked on development of an online portal for Sellers to report their sales and claim back-margin rebates, and for Admins to manages sellers and claims.</li>
+          <li>Implemented SQL procedure to update user information based on their contract details and last activity information.</li>
+          <li>Implemented a functionality to detect Fraudulent Claims and generated reports as per business requirements.</li>
+          <li>Worked on Front End and Backend Development of Admin Dashboard Screen.</li>
+          <li>Received 'Inspiring Performance Badge' from my manager as a token of appreciation.</li>
+          </ul>
+          `}
+        />
+        <WorkExperience
+          designation={"Project Engineer"}
+          company={"Wipro Technologies"}
+          location={"Chennai"}
+          image={"wipro.png"}
+          fromDate={new Date(2019, 8, 1)}
+          toDate={new Date(2020, 6, 1)}
           details={`
           <ul>
           <li>Worked on development of an online portal for Sellers to report their sales and claim back-margin rebates, and for Admins to manages sellers and claims.</li>
