4317b2201e8efd72a2d745cff65f47be499127d8
djay-S <65866198+djay-S@users.noreply.github.com>
2022-03-15 20:14:16 +0530

    code refactor

--- src/screens/aboutMe/aboutMe.js
+++ src/screens/aboutMe/aboutMe.js
@@ -3,7 +3,6 @@ import React from "react";
 import WorkExperience from "./workExperiences";
 import Certifications from "./certificates";
 import Skills from "./skills";
-import Education from "./education";
 import "./aboutMe.scss";
 
 export default function about_me() {
@@ -13,17 +12,16 @@ export default function about_me() {
         <h1>About Me</h1>
       </div>
       <div className="me content">
-        <text className="summary">
+        <section className="summary">
           I am seeking a position to utilise my skills and abilities that offers
           professional growth while being resourceful, innovative and flexible.
           I believe in team-work and I am open to learn new technologies that
           would help me in my growth as well as in the growth of the company as
           a whole.
-        </text>
+        </section>
         <Skills />
         <WorkExperience />
         <Certifications />
-        {/* <Education /> */}
       </div>
     </div>
   );
